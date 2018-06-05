69dc3926a1f19eda08b7c2e8327397224661293b
apw601 <apw601@gmail.com>
2018-06-05 19:31:29 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,6 +1,10 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if time>=11
+    puts "You're in trouble! Better get home quick!" 
+  end
 end
+
+puts simple_curfew_checker(3)
 
 def curfew_checker(time)
   # code goes here
